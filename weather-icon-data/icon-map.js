c4a9340dd9b3bd2a853781b8a57ac172bb2a62df
robertbrooks <robertbrooks1613@outlook.com>
2025-07-02 21:40:47 -0400

    update

--- weather-icon-data/icon-map.js
+++ weather-icon-data/icon-map.js
@@ -2,23 +2,23 @@
 export default function GetWeatherIcon(str) {
 
   const iconMap = {
-    "clear-day": "/weather-icon-data/clear-day.png",
-    "clear-night": "/weather-icon-data/clear-night.png",
-    "partly-cloudy-day": "/weather-icon-data/partly-cloudy-day.png",
-    "partly-cloudy-night": "/weather-icon-data/partly-cloudy-night.png",
-    "cloudy": "/weather-icon-data/cloudy.png",
-    "fog": "/weather-icon-data/fog.png",
-    "wind": "/weather-icon-data/wind.png",
-    "rain": "/weather-icon-data/rain.png",
-    "showers-day": "/weather-icon-data/showers-day.png",
-    "showers-night": "/weather-icon-data/showers-night.png",
-    "snow": "/weather-icon-data/snow.png",
-    "snow-showers-day": "/weather-icon-data/snow-showers-day.png",
-    "snow-showers-night": "/weather-icon-data/snow-showers-night.png",
-    "thunder": "/weather-icon-data/thunder.png",
-    "thunder-showers-day": "/weather-icon-data/thunder-showers-day.png",
-    "thunder-showers-night": "/weather-icon-data/thunder-showers-night.png",
-    "hail": "/weather-icon-data/hail.png"
+    "clear-day": "/top-weather-app/weather-icon-data/clear-day.png",
+    "clear-night": "/top-weather-app/weather-icon-data/clear-night.png",
+    "partly-cloudy-day": "/top-weather-app/weather-icon-data/partly-cloudy-day.png",
+    "partly-cloudy-night": "/top-weather-app/weather-icon-data/partly-cloudy-night.png",
+    "cloudy": "/top-weather-app/weather-icon-data/cloudy.png",
+    "fog": "/top-weather-app/weather-icon-data/fog.png",
+    "wind": "/top-weather-app/weather-icon-data/wind.png",
+    "rain": "/top-weather-app/weather-icon-data/rain.png",
+    "showers-day": "/top-weather-app/weather-icon-data/showers-day.png",
+    "showers-night": "/top-weather-app/weather-icon-data/showers-night.png",
+    "snow": "/top-weather-app/weather-icon-data/snow.png",
+    "snow-showers-day": "/top-weather-app/weather-icon-data/snow-showers-day.png",
+    "snow-showers-night": "/top-weather-app/weather-icon-data/snow-showers-night.png",
+    "thunder": "/top-weather-app/weather-icon-data/thunder.png",
+    "thunder-showers-day": "/top-weather-app/weather-icon-data/thunder-showers-day.png",
+    "thunder-showers-night": "/top-weather-app/weather-icon-data/thunder-showers-night.png",
+    "hail": "/top-weather-app/weather-icon-data/hail.png"
   };
 
   if (iconMap[str]) {
